7545162d895a41160935dc30e2e12872b5a2985d
Matteo Lopez <matteolopezbb@gmail.com>
2022-04-07 16:17:35 +0200

    inserimento img nel progetto, e creazione dinamica elementi navbar + inserimento logo image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,67 @@
 <template>
-    <div></div>
+    <header class="container">
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light mt-2">
+                    <div class="container-fluid">
+                        <img src="../assets/img/nexgen-logo.svg" class="img-fluid my-logo" alt="">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav mb-2 mb-lg-0 text-end">
+                                <li class="nav-item" v-for='(element, index) in navBar' :key="index">
+                                <a class="nav-link active" aria-current="page" href="#">{{element.item}}</a>
+                                </li>
+                            </ul>
+                            <form class="d-flex">
+                                <button class="btn btn-outline-success mx-3 my-button" type="submit">GET IN TOUCH</button>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
 </template>
 
 <script>
 export default {
-    name : 'IndexHeader'
+    name : 'IndexHeader',
+    data : function(){
+        return {
+            navBar : [
+                {
+                    item : 'HOME'
+                },
+                {
+                    item : 'ABOUT'
+                },
+                {
+                    item : 'SERVICES'
+                },
+                {
+                    item : 'PROCESS'
+                },
+                {
+                    item : 'TEAM'
+                },
+                {
+                    item : 'BLOG'
+                },
+            ]
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+    .my-button{
+        background-color: #048383;
+        color: white;
+    }
+    .my-logo{
+        height: 4vh;
+    }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -28,6 +28,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
 </style>
